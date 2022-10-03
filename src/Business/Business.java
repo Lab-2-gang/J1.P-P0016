32f5bfc92a8b655c1fc8f2000568c674aa27d665
vincentdaogithub <vincentdaomain@gmail.com>
2022-10-03 13:09:42 +0700

    Update Business.java

--- src/Business/Business.java
+++ src/Business/Business.java
@@ -1,5 +1,54 @@
 package Business;
 
-public class Business {
-    
+import java.util.Scanner;
+
+public class Business
+{
+    // scanner
+    private final Scanner readInput = new Scanner(System.in);
+
+
+    // read input as string
+    public String ReadInput()
+    {
+        try
+        {
+            return readInput.nextLine().trim();
+        }
+        catch (Exception e)
+        {
+            // cannot read
+            return null;
+        }
+    }
+
+
+    // convert input to int
+    public Integer convertToInteger(String input)
+    {
+        try
+        {
+            return Integer.parseInt(input);
+        }
+        catch (Exception e)
+        {
+            // cannot convert
+            return null;
+        }
+    }
+
+
+    // convert input 
+    public Float ConvertToFloat(String input)
+    {
+        try
+        {
+            return Float.parseFloat(input);
+        }
+        catch (Exception e)
+        {
+            // cannot convert
+            return null;
+        }
+    }
 }
