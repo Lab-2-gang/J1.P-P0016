0382306a5297a25cdf71545b10b5e42b695d0b5b
Hotuanhung852 <hotuan850@gmail.com>
2022-10-04 17:07:45 +0700

    Update BusinessDelegate.java

--- src/Business/BusinessDelegate.java
+++ src/Business/BusinessDelegate.java
@@ -1,10 +1,44 @@
 package Business;
 
 public class BusinessDelegate {
-    
+    // implement operations from enum class DealerOperationsType
+    // in a switch statement
+    public void Delegate(DealerOperationsType task) {
+        switch (task) {
+            
+            case Add:
+                // add a new dealer
+                break;
 
-    public void doTask(D
+            case Search:
+                // search for a dealer
+                break;
 
-}
+            case Delete:
+                // delete a dealer
+                break;
+
+            case Update:
+                // update a dealer
+                break;
+
+            case PrintAll:
+                // print all dealers
+                break;
+
+            case PrintContinuing:
+                // print continuing dealers
+                break;
+
+            case PrintUncontinuing:
+                // print uncontinuing dealers
+                break;
+
+            case WriteToFile:
+                // write dealers to file
+                break;
+
+        }
     }
+
 }
